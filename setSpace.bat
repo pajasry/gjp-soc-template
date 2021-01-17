90d4ee8e71117097ef6e9b0d662164bca5ac0570
Pavel Šrytr <pajasry@gmail.com>
2021-01-17 12:11:17 +0100

    Add files via upload

---
+++ setSpace.bat
@@ -0,0 +1,6 @@
+ vlna32.exe -l -f -v KkSsVvZzOoUuAaIivlna 00uvod.tex
+ vlna32.exe -l -f -v KkSsVvZzOoUuAaIivlna 01elektronika.tex
+ vlna32.exe -l -f -v KkSsVvZzOoUuAaIivlna 02navrh.tex
+ vlna32.exe -l -f -v KkSsVvZzOoUuAaIivlna 03software.tex
+ vlna32.exe -l -f -v KkSsVvZzOoUuAaIivlna 04vyuka.tex
+ vlna32.exe -l -f -v KkSsVvZzOoUuAaIivlna 05zaver.tex
